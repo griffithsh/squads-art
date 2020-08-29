f9743c9917cf42875a250798ae398a60ea80bd24
Huw Griffiths <huw.m.griffiths@gmail.com>
2020-08-29 21:13:10 +1000

    Add some trees

---
+++ combat-terrain/tree-v1.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tree-v1" tilewidth="256" tileheight="256" tilecount="4" columns="2">
+ <tileoffset x="-92" y="24"/>
+ <image source="tree-v1.png" width="512" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="Obscures">-1,-2
+-1,-3
+-1,-4
+0,-1
+0,-2
+0,-3
+0,-4
+1,-2
+1,-3
+1,-4</property>
+  </properties>
+ </tile>
+</tileset>
