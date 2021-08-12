bb385d6256edb9b066e9b2be01f1b16f07aee77c
Huw Griffiths <huw.m.griffiths@gmail.com>
2021-08-12 19:47:48 +1000

    Add podman fallback to docker script
    Docker doesn;t work on fedora, but podman does. This allows this script
    to work on systems that do not have docker installed.

--- docker.sh
+++ docker.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/sh -e
 
-# runs everything.sh in a docker container. Make sure you have the squads repo
+# runs everything.sh in a container. Make sure you have the squads repo
 # as a sibling directory of this one.
 
-docker build -t squads-art-build .
-docker run -i -t -v $(pwd):/mnt:z -v $(pwd)/../squads:/squads:z squads-art-build
+CONTAINER_EXE=docker
+if ! hash docker 2>/dev/null
+then
+	CONTAINER_EXE=podman
+fi
+
+$CONTAINER_EXE build -t squads-art-build .
+$CONTAINER_EXE run -i -t -v $(pwd):/mnt:z -v $(pwd)/../squads:/squads:z squads-art-build
